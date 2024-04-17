bfc3339bdd4ce2c4796d6d62054533b0710c4fac
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-04-17 21:25:57 +0200

    refactor AppData for better testing.

--- core-serv/src/main/kotlin/com/emat/coreserv/global/AppData.kt
+++ core-serv/src/main/kotlin/com/emat/coreserv/global/AppData.kt
@@ -2,15 +2,24 @@ package com.emat.coreserv.global
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.time.Clock
 import java.time.LocalDateTime
 
 @Component
 class AppData {
+    var clock: Clock = Clock.systemDefaultZone()
+        set(value) {
+            field = value
+        }
 
     @Value("\${app.version}")
     private lateinit var appVersion: String
 
+    private fun now(): LocalDateTime {
+        return LocalDateTime.now(clock)
+    }
+
     fun getApplicationVersion(): String {
-        return appVersion.plus(" on the day: ").plus(LocalDateTime.now().toString())
+        return appVersion.plus(" on the day: ").plus(now().toString())
     }
 }
