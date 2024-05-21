3e3afc1b251e8d67f0f49c1dfaae95c903b496d0
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-05-21 16:01:23 +0200

    add fist unit test and still is in progress, will be continue in antoher branch

--- api-gateway/src/test/kotlin/com/emat/apigateway/unit/global/SelfCheckControllerUnitTest.kt
+++ api-gateway/src/test/kotlin/com/emat/apigateway/unit/global/SelfCheckControllerUnitTest.kt
@@ -1,49 +1,63 @@
-//package unit.com.emat.apigateway.global
-//
-//import com.emat.apigateway.global.SelfCheckController
-//import com.emat.coreserv.global.AppData
-//import mu.KotlinLogging
-//import org.junit.jupiter.api.Assertions
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.TestInfo
-//import org.mockito.Mockito
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-//import org.springframework.boot.test.mock.mockito.MockBean
-//import org.springframework.test.web.reactive.server.WebTestClient
-//import java.time.Clock
-//import java.time.Instant
-//import java.time.ZoneOffset
-//
-//@WebMvcTest(controllers = [SelfCheckController::class])
-//class SelfCheckControllerUnitTest {
-//
-//    private val log = KotlinLogging.logger {}
-//
-//    @Autowired
-//    lateinit var webTestClient: WebTestClient
-//
-//    @MockBean
-//    private lateinit var appData: AppData
-//
-//    @BeforeEach
-//    fun setup(testInfo: TestInfo) {
-//        log.info("Starting test: ${testInfo.displayName}")
-//    }
-//
-//    @Test
-//    fun `retrieve application version returns version as expected and use GET method, returns 200 response code`() {
-//        //given
-//        val applicationVersion = "1.0.0_SNAPSHOT".plus(" on the day: ").plus("2024-10-15T00:00")
-//
-//        //when
-//        appData.clock = Clock.fixed(Instant.parse("2024-10-15T00:00:00Z"), ZoneOffset.UTC)
-//        Mockito.`when`(appData.getApplicationVersion()).thenReturn(applicationVersion)
-//
-//        //then
-//        webTestClient.get().uri("/v1/selfcheck/version")
-//            .exchange()
+package com.emat.apigateway.global
+
+import com.emat.apigateway.transcript.TranscriptController
+import com.emat.coreserv.global.AppData
+import io.mockk.every
+import io.mockk.mockk
+import mu.KotlinLogging
+import org.junit.jupiter.api.*
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneOffset
+
+class SelfCheckControllerUnitTest {
+
+    private val log = KotlinLogging.logger {}
+
+    private lateinit var mockMvc: MockMvc
+    private lateinit var appData: AppData
+
+    @BeforeEach
+    fun setup(testInfo: TestInfo) {
+        log.info("Starting test: ${testInfo.displayName}")
+
+        appData = mockk()
+
+        mockMvc = MockMvcBuilders.standaloneSetup(SelfCheckController(appData))
+            .setControllerAdvice(GlobalControllerExceptionHandler::class)
+            .build()
+    }
+
+    @AfterEach
+    fun tearDown(testInfo: TestInfo) {
+        log.info("Finished test: ${testInfo.displayName}")
+    }
+
+
+    @Test
+    fun `retrieve application version returns version as expected and use GET method, returns 200 response code`() {
+        //given
+        val applicationVersion = "1.0.0_SNAPSHOT".plus(" on the day: ").plus("2024-10-15T00:00")
+
+        //when
+        val fixedClock = Clock.fixed(Instant.parse("2024-10-15T00:00:00Z"), ZoneOffset.UTC)
+        every { appData.clock } returns fixedClock
+        every { appData.getApplicationVersion() } returns applicationVersion
+
+        //expect
+        mockMvc.get("/v1/selfcheck/version")
+            .andExpect {
+                status { isOk() }
+                content { string(applicationVersion) }
+            }
 //            .expectStatus().isOk
 //            .expectBody(String::class.java)
 //            .consumeWith { response ->
@@ -51,8 +65,9 @@
 //                Assertions.assertEquals(applicationVersion, response.responseBody)
 //                Assertions.assertNotEquals("Other verrsion", response.responseBody)
 //            }
-//    }
-//
+
+    }
+
 //    @Test
 //    fun `greetings endpoint returns expected message and use GET method returns 200 response code`() {
 //        //given
@@ -72,6 +87,6 @@
 //        Assertions.assertEquals("Hello $testName", resposne.responseBody)
 //
 //    }
-//
-//
-//}
+
+
+}
